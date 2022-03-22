cbcf8d0ec6acf2523beb0f12eaafbb9a50fe3d77
Samuelr499 <samuelribeiro2@hotmail.com>
2022-03-22 17:34:41 -0300

    resolver ate o requisito 7

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const listaPai = document.createElement('ol');
-listaPai.id ='lista-tarefas';
+listaPai.id = 'lista-tarefas';
 
 document.body.appendChild(listaPai);
 
@@ -10,11 +10,22 @@ btn.innerText = 'New Achievement'
 
 let = textBox = document.getElementById('texto-tarefa');
 // console.log(textBox);
-function adicionaTarefa(){
-const tarefa = document.createElement('li');
-tarefa.innerText = textBox.value; 
-listaPai.appendChild(tarefa);
-textBox.value = '';
+function adicionaTarefa() {
+    const tarefa = document.createElement('li');
+    tarefa.innerText = textBox.value;
+    listaPai.appendChild(tarefa);
+    textBox.value = '';
 }
 
 btn.addEventListener('click', adicionaTarefa);
+
+// function tarefaSelect(event) {
+//     event.target.classList.add('selected');
+
+// }
+function newselect(evnt) {
+    evnt.target.classList.add('selected');
+}
+
+// listaPai.addEventListener('click', tarefaSelect);
+listaPai.addEventListener('click', newselect);
