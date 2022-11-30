8d24778b7d7e89776fef744b04ef761ed961af3d
Samuelr499 <samuelribeiro2@hotmail.com>
2022-11-29 21:17:30 -0300

    fix: erros de lint

--- script.js
+++ script.js
@@ -15,57 +15,52 @@ btnFinlzd.id = 'remover-finalizados';
 document.body.appendChild(btnFinlzd);
 btnFinlzd.innerText = ' Limpar finalizados ';
 
-let = textBox = document.getElementById('texto-tarefa');
+const textBox = document.getElementById('texto-tarefa');
 function adicionaTarefa() {
-    const tarefa = document.createElement('li');
-    tarefa.innerText = textBox.value;
-    listaPai.appendChild(tarefa);
-    textBox.value = '';
+  const tarefa = document.createElement('li');
+  tarefa.innerText = textBox.value;
+  listaPai.appendChild(tarefa);
+  textBox.value = '';
 }
 
 btn.addEventListener('click', adicionaTarefa);
 
-
 function newselect(evnt) {
-    const tarefa = document.querySelector('.selected');
-
-    if (tarefa !== null) {
-        tarefa.classList.remove('selected');
-    }
-    evnt.target.classList.add('selected');
+  const tarefa = document.querySelector('.selected');
+  if (tarefa !== null) {
+    tarefa.classList.remove('selected');
+  }
+  evnt.target.classList.add('selected');
 }
-
 
 listaPai.addEventListener('click', newselect);
 
 function finalizado(evnt) {
-
-    evnt.target.classList.toggle('completed');
-    // console.log('double click');
-    // if(evnt.target.classList.contains('completed') ){
-    //     evnt.target.classList.remove('completed');
-    // }else{
-    //     evnt.target.classList.add('completed');
-    // }
+  evnt.target.classList.toggle('completed');
+  // console.log('double click');
+  // if(evnt.target.classList.contains('completed') ){
+  //     evnt.target.classList.remove('completed');
+  // }else{
+  //     evnt.target.classList.add('completed');
+  // }
 }
 
 listaPai.addEventListener('dblclick', finalizado);
 
 function faxina() {
-    listaPai.innerHTML = '';
+  listaPai.innerHTML = '';
 }
 
 btnClear.addEventListener('click', faxina);
 
 function tchauFinalizados() {
-    const concluidas = document.querySelectorAll('.completed');
-    for (let i = 0; i < concluidas.length; i++) {
-        concluidas[i].remove();
-    }
+  const concluidas = document.querySelectorAll('.completed');
+  for (let i = 0; i < concluidas.length; i += 1) {
+    concluidas[i].remove();
+  }
 }
 btnFinlzd.addEventListener('click', tchauFinalizados);
 
 // function save() {
 // listaOrdenada = document.getElementById('lista-tarefas')
 // }
-
